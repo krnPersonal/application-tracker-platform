653ca95a8b6050b31065e47305faf6848032ab00
Khem R Neupane <khemneupane84@gmail.com>
2026-04-16 22:06:55 -0500

    Initialize spring boot project structure

---
+++ backend/src/test/java/com/applicationtracker/ApplicationTrackerTests.java
@@ -0,0 +1,13 @@
+package com.applicationtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
